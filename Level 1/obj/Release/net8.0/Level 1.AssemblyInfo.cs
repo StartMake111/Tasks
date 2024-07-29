69e8420f18e25bc5f1325b5feb8c8a8ae421c0ef
SMLiJeck <68592362+StartMake111@users.noreply.github.com>
2024-07-29 14:17:28 +0300

    exe

--- Level 1/obj/Release/net8.0/Level 1.AssemblyInfo.cs
+++ Level 1/obj/Release/net8.0/Level 1.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Level 1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cec84be9afa8f9727e4451ee9227d7d3c77f515")]
 [assembly: System.Reflection.AssemblyProductAttribute("Level 1")]
